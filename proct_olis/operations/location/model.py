b0295cf3cddc90b30a4af29ddd431b436f703746
kaljob <90810703+kaljob@users.noreply.github.com>
2025-11-13 14:39:26 -0500

    feat: add table transactional | location (#9)
    Co-authored-by: Kaljob Martial <kaljobmartial@kamez.ca>

---
+++ proct_olis/operations/location/model.py
@@ -0,0 +1,30 @@
+from proct_olis.core import TransformationBase 
+import polars as pl
+
+
+class Transformation(TransformationBase):
+    process_name: str = "Location Transactional Database Process"
+
+    CITY_MAP = {
+        "trajano de morais": "trajano de moraes",
+        "senador la roque": "senador la rocque",
+        "buritirana": "buritirama",
+        "estrela d oeste": "estrela d’oeste",
+        "estrela doeste": "estrela d’oeste",
+    }
+
+    def transformation(self):
+        location_df = (
+            self.entity_map.get("datalake.ref_geolocation")
+            .select(
+                pl.col("zip_code_prefix"),
+                pl.col("city").str.to_lowercase().map_elements(self.utilities.remove_accents).alias("city"),
+                pl.col("state").str.to_lowercase().alias("state"),
+            )
+            .with_columns(
+                pl.col("city").replace(self.CITY_MAP, default=pl.col("city")).alias("city")
+            )
+            .unique()
+        )
+
+        self.final_df = self.utilities.add_primary_key(location_df, "auto", self.config.destination.primary_key, self.config.destination.business_keys)
