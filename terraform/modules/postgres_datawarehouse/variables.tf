f4537344b2aacdcdd01edd23bd7f18a0fd5700be
rvfiiik123 <benchiharafik@gmail.com>
2025-11-27 00:06:37 +0000

    feat: postegresdatawarehouse

---
+++ terraform/modules/postgres_datawarehouse/variables.tf
@@ -0,0 +1,23 @@
+variable "db_host" {
+    type    = string
+    description = "PostgreSQL database host"
+    default = "postgres_operations"
+}
+variable "db_port" {
+    type    = number
+    description = "PostgreSQL database port"
+    default = 5432
+}
+
+variable "db_admin_user" {
+    type    = string
+    description = "PostgreSQL database admin user"
+    default = "admin"
+}
+
+variable "db_admin_password" {
+    type    = string
+    description = "PostgreSQL database admin password"
+    default = "admin"
+}
+
